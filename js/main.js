86751019193c76cad5abf4a48c9f3b7fa9526130
Micaela Frylingsztein <micafry@gmail.com>
2023-03-26 12:15:10 -0300

    librerias agregadas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const horasDisponibles = ["11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"];
 const turnosOcupados = [];
 
@@ -46,27 +47,60 @@ formulario.addEventListener('submit', (sacarTurno) => {
     const dia = diaInput.value;
     const hora = horaInput.value;
 
-    const cliente = function (nombre, apellido,telefono,email){
-        this.nombre=nombre,
-        this.apellido=apellido,
-        this.telefono=telefono,
-        this.email=email
+    const cliente = function (nombre, apellido, telefono, email, dia, hora) {
+        this.nombre = nombre,
+            this.apellido = apellido,
+            this.telefono = telefono,
+            this.email = email,
+            this.dia = dia,
+            this.hora=hora
     }
 
-const cliente1= new cliente(nombre, apellido, telefono, email);
-localStorage.setItem('datos del cliente', JSON.stringify(cliente1))
+    const cliente1 = new cliente(nombre, apellido, telefono, email, dia,hora);
+    localStorage.setItem('datos del cliente', JSON.stringify(cliente1))
+    const datosCliente = localStorage.getItem('datos del cliente')
+    const datos = JSON.parse(datosCliente);
+
+    const confirmacion = swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-primary'
+        },
+        buttonsStyling: false
+    })
+
+    confirmacion.fire({
+        title: '¿Desea confirmar los datos ingresados?',
+        text: `Nombre: ${nombre} ${apellido},  Teléfono: ${telefono}, Email: ${email} Turno: ${dia} ${hora}`, 
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar'
+    })
+    .then((resultado) => {
+        if (resultado.value) {
+            if (reservarHora(dia, hora, nombre, apellido, telefono, email)) {
+                mensaje.textContent = `Gracias ${nombre} ${apellido}! Su turno ha sido reservado para el día ${dia} a las ${hora}.`;
+                swal.fire({
+                    title: 'Turno reservado con éxito',
+                    icon: 'success'
+                })
+                nombreInput.value = '';
+                apellidoInput.value = '';
+                emailInput.value = '';
+                telefonoInput.value = '';
+                diaInput.value = '';
+                horaInput.value = '';
+            } else {
+                mensaje.textContent = `Lo sentimos, el turno del ${dia} a las ${hora} ya está reservado. Por favor, seleccione otra fecha/hora.`;
+                swal.fire({
+                    title: 'Turno ocupado',
+                    text: 'Por favor, seleccione otro turno.',
+                    icon: 'error'
+                })
+            }
+        }
+    })
+});
 
 
-
-    if (reservarHora(dia, hora, nombre, apellido, telefono, email)) {
-        mensaje.innerText = `Gracias ${nombre} ${apellido}! Su turno ha sido reservado para el día ${dia} a las ${hora}.`;
-        nombreInput.value = '';
-        apellidoInput.value = '';
-        emailInput.value = '';
-        telefonoInput.value = '';
-        diaInput.value = '';
-        horaInput.value = '';
-    } else {
-        mensaje.innerText = `Lo sentimos, el turno del ${dia} a las ${hora} ya está reservado. Por favor, seleccione otra fecha/hora.`;
-}});
 
